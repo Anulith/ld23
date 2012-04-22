d82c3caefe384a500e9368c1905079d379e3ade9
Josh <josh@josh-laptop.(none)>
2012-04-22 03:01:46 -0400

    Woops, didn't add bacteria.lua in last commit

---
+++ nanomd/bacteria.lua
@@ -0,0 +1,50 @@
+Bacteria = class()
+
+function Bacteria:__init(x, y)
+	self.x = x
+	self.y = y
+	self.xspeed = math.random(-50, 50)
+	self.yspeed = math.random(-50, 50)
+	self.width = bacteriaGfx:getWidth()
+	self.height = bacteriaGfx:getHeight()
+	self.offsetX = self.width/2
+	self.offsetY = self.height/2
+    self.r = math.random(0.0, 6.28)
+end
+
+function Bacteria:update(dt)
+	self.x = self.x + self.xspeed * dt
+	self.y = self.y + self.yspeed * dt
+	if self.x < 0 then
+		self.x = 0
+		self.xspeed = -self.xspeed
+	elseif self.x > (800 - self.width) then
+		self.x = 800 - self.width
+		self.xspeed = -self.xspeed
+	end
+	if self.y < 0 then
+		self.y = 0
+		self.yspeed = -self.yspeed
+	elseif self.y > (600 - self.height) then
+		self.y = 600 - self.height
+		self.yspeed = -self.yspeed
+	end
+	for i,p in pairs(projectiles) do
+		if(collides(self, p)) then
+			table.remove(projectiles, i)
+            score = score + 20
+			return false
+		end
+	end
+    if(collides(self, player:getRect())) then
+        player:destroy()
+        return false
+    end
+	return true
+end
+
+function Bacteria:draw()
+	--scale = math.random(0.5, 2)
+	scale = 1
+	love.graphics.draw(bacteriaGfx, self.x, self.y, self.r)
+end
